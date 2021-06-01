0e3ee2f34a3479d163d678fef163e0fe3b309b8d
lauren-briggs <laurenbriggs41@gmail.com>
2021-06-01 21:06:44 +1000

    added session npms to dependencies and initial code for user login

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const path = require("path");
 const express = require("express");
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const routes = require('./controllers')
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'secret',
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
