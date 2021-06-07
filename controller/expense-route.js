314af598f90ebfd2d35c102dde13302d0699ff16
Dylan Rooney <dylantrooney1@gmail.com>
2021-06-07 21:13:52 +1000

    added grid page and handlebars

--- controller/expense-route.js
+++ controller/expense-route.js
@@ -1,39 +1,31 @@
-const router = require('express').Router();
-const { Expense } = require('../models');
+const router = require("express").Router();
+const { Expense } = require("../models");
 
-router.get('/expense/:id', async (req, res) => {
-    try {
-        const expensedata = Expense.findAll({ where: user_id = req.params.id })
-        res.status(204).json(expensedata)
-        //maybe write logic to turn into array then use client side logic to put onto page
-    } catch (err) {
-        res.status(500).json(err)
+router.get("/expense/:id", async (req, res) => {
+  try {
+    const expensedata = Expense.findAll({ where: (user_id = req.params.id) });
+    res.status(204).json(expensedata);
+    //maybe write logic to turn into array then use client side logic to put onto page
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+router.post("/expense", async (req, res) => {
+  console.log("request received");
+  console.log(req.body.expense_name);
+  try {
+    const newexpense = Expense.create({
+      //do i need id?
+      expense_name: req.body.expense_name,
+      amount: req.body.amount,
+      sub_category_id: req.body.sub_category_id,
+      user_id: req.body.user_id,
+    });
+    res.status(200).json(newexpense);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-    }
-}
-)
-router.post('/expense', async (req, res) => {
-    console.log('request received')
-    console.log(req.body.expense_name)
-    try {
-        const newexpense = Expense.create({
-            //do i need id?
-            expense_name: req.body.expense_name,
-            amount: req.body.amount,
-            sub_category_id: req.body.sub_category_id,
-            user_id: req.body.user_id
-
-        })
-        res.status(200).json(newexpense)
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err)
-    }
-
-})
-
-
-
-
-
-module.exports = router
+module.exports = router;
--- controller/htmlroutes.js
+++ controller/htmlroutes.js
@@ -1,40 +1,47 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', async (req, res,) => {
-    try {
-        if (req.session.loggedIn) {
-            res.render('profile', { loggedIn: req.session.loggedIn })
-        } else {
-            res.render('login', { loggedIn: req.session.loggedIn })
-        }
-    } catch (err) {
-        res.status(500).json(err)
+router.get("/", async (req, res) => {
+  try {
+    if (req.session.loggedIn) {
+      res.render("profile", { loggedIn: req.session.loggedIn });
+    } else {
+      res.render("login", { loggedIn: req.session.loggedIn });
     }
-})
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.get('/signup', async (req, res,) => {
-    try {
-        res.render('signup')
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.get('/profile', async (req, res) => {
-    try {
-        res.render('profile', { loggedIn: req.session.loggedIn });
-    } catch {
-        res.status(500).json(err)
-    }
-})
+router.get("/profile", async (req, res) => {
+  try {
+    res.render("profile", { loggedIn: req.session.loggedIn });
+  } catch {
+    res.status(500).json(err);
+  }
+});
 
-router.get('/expense', async (req, res) => {
-    try {
-        res.render('expense', { loggedIn: req.session.loggedIn });
-    } catch {
-        res.status(500).json(err)
-    }
-})
+router.get("/expense", async (req, res) => {
+  try {
+    res.render("expense", { loggedIn: req.session.loggedIn });
+  } catch {
+    res.status(500).json(err);
+  }
+});
 
+router.get("/view", async (req, res) => {
+  try {
+    res.render("view", { loggedIn: req.session.loggedIn });
+  } catch {
+    res.status(500).json(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
