4844df5c142129331772df991b584ee219ac779b
ReubenFev <reuben.fevy@gmail.com>
2021-06-07 20:07:35 +1000

    making expense route work

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,13 @@
 const sequelize = require('../config/connection');
 const seedParentCategory = require('./parentCategoryData');
 const seedSubCategory = require('./subCategoryData');
+const seeddummydata = require('./dummyuserdata')
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
     await seedParentCategory();
     await seedSubCategory();
+    await seeddummydata();
 
     process.exit(0);
 };
