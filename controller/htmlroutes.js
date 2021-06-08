f8ccdca9148604dd53c8a96d0d0f372686f075c1
tommylu780 <nhoksun340@gmail.com>
2021-06-08 19:01:38 +1000

    fix route for view expense

--- controller/htmlroutes.js
+++ controller/htmlroutes.js
@@ -1,47 +1,47 @@
 const router = require("express").Router();
 
-router.get("/", async (req, res) => {
-  try {
-    if (req.session.loggedIn) {
-      res.render("profile", { loggedIn: req.session.loggedIn });
-    } else {
-      res.render("login", { loggedIn: req.session.loggedIn });
+router.get("/", async(req, res) => {
+    try {
+        if (req.session.loggedIn) {
+            res.render("profile", { loggedIn: req.session.loggedIn });
+        } else {
+            res.render("login", { loggedIn: req.session.loggedIn });
+        }
+    } catch (err) {
+        res.status(500).json(err);
     }
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.get("/signup", async (req, res) => {
-  try {
-    res.render("signup");
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get("/signup", async(req, res) => {
+    try {
+        res.render("signup");
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get("/profile", async (req, res) => {
-  try {
-    res.render("profile", { loggedIn: req.session.loggedIn });
-  } catch {
-    res.status(500).json(err);
-  }
+router.get("/profile", async(req, res) => {
+    try {
+        res.render("profile", { loggedIn: req.session.loggedIn });
+    } catch {
+        res.status(500).json(err);
+    }
 });
 
-router.get("/expense", async (req, res) => {
-  try {
-    res.render("expense", { loggedIn: req.session.loggedIn });
-  } catch {
-    res.status(500).json(err);
-  }
+router.get("/expense", async(req, res) => {
+    try {
+        res.render("expense", { loggedIn: req.session.loggedIn });
+    } catch {
+        res.status(500).json(err);
+    }
 });
 
-router.get("/view", async (req, res) => {
-  try {
-    res.render("view", { loggedIn: req.session.loggedIn });
-  } catch {
-    res.status(500).json(err);
-  }
+router.get("/add", async(req, res) => {
+    try {
+        res.render("add", { loggedIn: req.session.loggedIn });
+    } catch {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
