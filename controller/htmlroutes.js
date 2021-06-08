06965c57a8575f1d55e76b080c674ff467f8632a
lauren-briggs <laurenbriggs41@gmail.com>
2021-06-08 20:53:21 +1000

    user expenses connected to expense route

--- controller/htmlroutes.js
+++ controller/htmlroutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { SubCategory } = require('../models');
+const { SubCategory, Expense } = require('../models');
 
 
 router.get("/", async (req, res) => {
@@ -32,7 +32,16 @@ router.get("/profile", async (req, res) => {
 
 router.get("/expense", async (req, res) => {
     try {
-        res.render("expense", { loggedIn: req.session.loggedIn });
+        const expenseData = await Expense.findAll({
+            where: {
+                user_id: req.session.user_id,
+            }
+        }).catch((err) => {
+            res.json(err);
+        })
+        const expenses = expenseData.map((expense_name) => expense_name.get({ plain: true }));
+        console.log(expenses);
+        res.render("expense", { expenses, loggedIn: req.session.loggedIn });
     } catch {
         res.status(500).json(err);
     }
