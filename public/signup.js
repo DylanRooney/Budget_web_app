55f9dcea7e4eb8be2818b09a99346e36e2970c10
lauren-briggs <laurenbriggs41@gmail.com>
2021-06-09 13:22:53 +1000

    render username and email on profile screen

--- public/signup.js
+++ public/signup.js
@@ -30,7 +30,6 @@ async function handleSignup(event) {
                 ul.appendChild(li);
             })
             errorsEl.appendChild(ul);
-            // alert(response.statusText);
         }
     }
 }
