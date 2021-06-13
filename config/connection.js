8a19abddd330c5192d21dbfca7e4284b17c38ad7
Dylan Rooney <dylantrooney1@gmail.com>
2021-06-13 22:37:06 +1000

    installed Jaws DB connection

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,19 @@ const path = require("path");
 const Sequelize = require("sequelize");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL) {
+  sequelize = new sequelize(process.env.JAWSDB_URL);
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "qao3ibsa7hhgecbv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
